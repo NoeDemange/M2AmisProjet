ebe4fc858389fe1957dfcc91e2cf800c3e65921a
Anne Fernet <anne.fernet@ent.uvsq.fr>
2024-02-23 23:29:14 +0100

    modif variables clique.c

--- src/clique.c
+++ src/clique.c
@@ -10,9 +10,9 @@ long chrono_clique() {
 }
 
 // Calcul récursif de la clique max
-void calcul_cl(grapheSim *g, int* dans_clique_max, int* taille_clique_max, int *dans_clique,int taille_clique,int *candidat,int taille_candidat, long maxdate) {	
+void calcul_cl(grapheSim *g, int* dans_clique_maxR, int* taille_clique_maxR, int *dans_cliqueX,int taille_cliqueX,int *candidatP,int taille_candidatP, long maxdate) {	
 	
-  int taille = g->nb_sommets;
+  int nb_sommets = g->nb_sommets;
   
 	if (maxdate != 0) {	
 		if(chrono_clique()  > maxdate) 
@@ -20,40 +20,40 @@ void calcul_cl(grapheSim *g, int* dans_clique_max, int* taille_clique_max, int *
 	}
 	int i,j;
 
-	if( taille_candidat == 0) {
-		if(taille_clique > *taille_clique_max) {
-			*taille_clique_max = taille_clique;
-			for (i = 0 ;  i < taille ; i ++) {
-				dans_clique_max[i] = dans_clique[i];
+	if( taille_candidatP == 0) {
+		if(taille_cliqueX > *taille_clique_maxR) {
+			*taille_clique_maxR = taille_cliqueX;
+			for (i = 0 ;  i < nb_sommets ; i ++) {
+				dans_clique_maxR[i] = dans_cliqueX[i];
 			}
 		}
 		return;
 	}
-	if (taille_candidat + taille_clique <= *taille_clique_max) {
+	if (taille_candidatP + taille_cliqueX <= *taille_clique_maxR) {
 		return;
 	}
 	
 	int taille_candidat_temp;
 	int *candidat_temp;
-	candidat_temp = malloc(taille * sizeof(int));
+	candidat_temp = malloc(nb_sommets * sizeof(int));
 
-	for (i = 0 ;  i < taille ; i ++) {
-		if ( candidat[i] == 1) {
-			candidat[i] = 0;
-			dans_clique[i] = 1 ;
-			taille_candidat_temp = taille_candidat;
+	for (i = 0 ;  i < nb_sommets ; i ++) {
+		if ( candidatP[i] == 1) {
+			candidatP[i] = 0;
+			dans_cliqueX[i] = 1 ;
+			taille_candidat_temp = taille_candidatP;
 			
-			for (j = 0 ;  j < taille ; j ++) {
-				candidat_temp[j] = candidat[j]; 
-				if ((candidat[j] == 1) && (g->adjacence[i][j] == 0)) {
+			for (j = 0 ;  j < nb_sommets ; j ++) {
+				candidat_temp[j] = candidatP[j]; 
+				if ((candidatP[j] == 1) && (g->adjacence[i][j] == 0)) {
 					candidat_temp[j] = 0;
 					taille_candidat_temp--;	
 				}	
 			}
 			
 			taille_candidat_temp --;
-			calcul_cl(g,dans_clique_max,taille_clique_max,dans_clique,taille_clique + 1,candidat_temp,taille_candidat_temp,maxdate);
-			dans_clique[i] = 0;
+			calcul_cl(g,dans_clique_maxR,taille_clique_maxR,dans_cliqueX,taille_cliqueX + 1,candidat_temp,taille_candidat_temp,maxdate);
+			dans_cliqueX[i] = 0;
 		}	
 	}
 	free(candidat_temp);
@@ -62,38 +62,38 @@ int* cliqueMax(grapheSim *g, long timeout) {
 
 	// Initialisation
 	int i;
-	int *candidat;
-	int *dans_clique;
+	int *candidatP;
+	int *dans_cliqueX;
   int taille_clique_max;
-	int *dans_clique_max;
+	int *dans_clique_maxR;
 
   int taille =g->nb_sommets;
 	
   taille_clique_max = 0;
 
-  dans_clique_max = malloc(taille * sizeof(int));
-	if (!dans_clique_max) {
-		fprintf(stderr,"cannot malloc dans_clique_max %d\n",taille); exit(41); 
+  dans_clique_maxR = malloc(taille * sizeof(int));
+	if (!dans_clique_maxR) {
+		fprintf(stderr,"cannot malloc dans_clique_maxR %d\n",taille); exit(41); 
 	}
-  dans_clique = malloc(taille *sizeof(int));
-	if (!dans_clique) {
-		fprintf(stderr,"cannot malloc dans_clique %d\n",taille); exit(42);
+  dans_cliqueX = malloc(taille *sizeof(int));
+	if (!dans_cliqueX) {
+		fprintf(stderr,"cannot malloc dans_cliqueX %d\n",taille); exit(42);
 	}
-	candidat = malloc( taille *sizeof(int));
-	if (!candidat) {
-		fprintf(stderr,"cannot malloc candidat %d\n",taille); exit(43);
+	candidatP = malloc( taille *sizeof(int));
+	if (!candidatP) {
+		fprintf(stderr,"cannot malloc candidatP %d\n",taille); exit(43);
 	}
 	
 	for (i = 0; i < taille ; i++) {
-		candidat[i] 	= 1;
-		dans_clique[i]	= 0;
-		dans_clique_max[i] = 0;
+		candidatP[i] 	= 1;
+		dans_cliqueX[i]	= 0;
+		dans_clique_maxR[i] = 0;
 	}
 	
 	// 0 taille de la clique initial  et m.nb_atome = nb sommets candidats
-	calcul_cl(g,dans_clique_max,&taille_clique_max,dans_clique,0,candidat,taille,chrono_clique() + timeout);
+	calcul_cl(g,dans_clique_maxR,&taille_clique_max,dans_cliqueX,0,candidatP,taille,chrono_clique() + timeout);
    
-	free(dans_clique);
-	free(candidat);
-  return dans_clique_max;
+	free(dans_cliqueX);
+	free(candidatP);
+  return dans_clique_maxR;
 }
